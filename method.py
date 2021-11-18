a5ccf1885c1e118ae7517af86c344b19a70ae2c1
NourhanElyamany <68388481+NourhanElyamany@users.noreply.github.com>
2021-11-18 23:37:07 +0200

    derrivative function

--- method.py
+++ method.py
@@ -1,7 +1,30 @@
 # inputs : equation function and 1 point
+from sympy import Symbol, Derivative
+import sympy as sym
+
+
 def func(expr, x):
     return eval(expr)
 
+def deriv():
+    
+    deriv= Derivative(function, x)
+    derivVal = deriv.doit().subs({x:4})
+    return derivVal
+
+    
+
+
 input_expr = input('Enter an expression in x: ')
-val = 4
-print(func(input_expr, val))
+
+# print(func(input_expr, val))
+
+
+
+# x = sym.Symbol('x')
+# function = input_expr
+# derivitive = sym.diff(function,2)
+# print(derivitive)
+
+
+
