9bab902dc002728cdd8ac9ab3e40a4166c87466d
karimkohel <kareemkohel@gmail.com>
2021-11-26 11:55:27 +0200

    better input handling

--- expandableNewton.py
+++ expandableNewton.py
@@ -3,8 +3,15 @@ import sympy as sp
 # inits for later use
 x = sp.Symbol('x')
 
-# getting the input fx
+# getting the input fx and cleaning it
 inputText = input("Enter the fx: ")
+inputText = inputText.replace('^', '**')
+
+xk = int(input("Initial point of starting: "))
+iterations = input("How many iterations do you need: ")
+
+
+# deriving the functions
 fx = eval(inputText, {'x': x, 'sin': sp.sin, 'cos': sp.cos, 'e': sp.E})
 fxDash = sp.diff(fx, x)
 
