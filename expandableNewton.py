2a6a6c71e5026d96727f2f178fe0e7470957423a
karimkohel <kareemkohel@gmail.com>
2021-11-26 13:54:31 +0200

    handling corner cases

--- expandableNewton.py
+++ expandableNewton.py
@@ -18,10 +18,20 @@ fxDash = sp.diff(fx, x)
 print(fx)
 print(fxDash)
 
-for _ in range(iterations):
+for i in range(iterations):
     # substituting the values in the functions to get a numerical answer and rounding it
     fxAns = sp.N(fx.subs(x, xk))
     fxDashAns = sp.N(fxDash.subs(x, xk))
+    roundedFX = "%.4f" % fxAns
+    roundedFXDash = "%.4f" % fxDashAns
+
+    if roundedFX == '0.0000':
+        print(f"achieved root with @ x = {xk} after {i+1} iterations")
+        break
+    elif roundedFXDash == '0.0000':
+        print(f"Newton's method cannot deal with flat spots, slope is 0 @ x = {xk} and iteration {i+1}")
+        break
+    # still need to handle runaway and cyclic
 
     # getting the new point
     xk = xk - (fxAns / fxDashAns)
