ff458eb8a0290afe65937efcaedc63e73c10fbd0
karimkohel <kareemkohel@gmail.com>
2021-11-26 12:08:42 +0200

    even better input and rounding for xk with precise numerical values

--- expandableNewton.py
+++ expandableNewton.py
@@ -8,7 +8,7 @@ inputText = input("Enter the fx: ")
 inputText = inputText.replace('^', '**')
 
 xk = int(input("Initial point of starting: "))
-iterations = input("How many iterations do you need: ")
+iterations = int(input("How many iterations do you need: "))
 
 
 # deriving the functions
@@ -17,3 +17,15 @@ fxDash = sp.diff(fx, x)
 
 print(fx)
 print(fxDash)
+
+for _ in range(iterations):
+    # substituting the values in the functions to get a numerical answer and rounding it
+    fxAns = sp.N(fx.subs(x, xk))
+    fxDashAns = sp.N(fxDash.subs(x, xk))
+
+    # getting the new point
+    xk = xk - (fxAns / fxDashAns)
+    xk = round(xk, 4)
+
+    print(xk)
+
